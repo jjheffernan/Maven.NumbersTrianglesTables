71d3d4498e50ee89ac5e8815b2f2dd199a46dfd4
jjheffernan <jjheffernan99@gmail.com>
2022-06-27 21:01:11 -0400

    fixed getLarge.. and getSmall.. to call getMultiplication table. passed tests

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,6 +3,41 @@ import java.lang.StringBuilder;
 import java.util.Scanner;
 
 public class TableUtilities {
+
+    public static String getSmallMultiplicationTable() {
+        return(getMultiplicationTable(5)); 
+    }
+
+    public static String getLargeMultiplicationTable() {
+        return(getMultiplicationTable(10));
+    }
+/*
+   Expected Test Case
+                "  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 |\n" +
+                "  2 |  4 |  6 |  8 | 10 | 12 | 14 | 16 | 18 | 20 |\n" +
+                "  3 |  6 |  9 | 12 | 15 | 18 | 21 | 24 | 27 | 30 |\n" +
+                "  4 |  8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40 |\n" +
+                "  5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 |\n" +
+                "  6 | 12 | 18 | 24 | 30 | 36 | 42 | 48 | 54 | 60 |\n" +
+                "  7 | 14 | 21 | 28 | 35 | 42 | 49 | 56 | 63 | 70 |\n" +
+                "  8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 72 | 80 |\n" +
+                "  9 | 18 | 27 | 36 | 45 | 54 | 63 | 72 | 81 | 90 |\n" +
+                " 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 |100 |\n";
+ */
+    public static String getMultiplicationTable(int tableSize) {
+
+        StringBuilder str = new StringBuilder();
+        for(int i = 1; i <= tableSize; i++)
+        {
+            for (int j = 1; j <=tableSize; j++) {
+                str.append(String.format("%3d |", i * j)); // has to be 3 digits to pass test
+            }
+            str.append("\n");
+        }
+        return str.toString();
+
+    }
+    /* legacy code
     public static String getSmallMultiplicationTable() {
 
         int num = 1;
@@ -26,30 +61,5 @@ public class TableUtilities {
         }
         return null;
     }
-/*
-   Expected Test Case
-                "  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 |\n" +
-                "  2 |  4 |  6 |  8 | 10 | 12 | 14 | 16 | 18 | 20 |\n" +
-                "  3 |  6 |  9 | 12 | 15 | 18 | 21 | 24 | 27 | 30 |\n" +
-                "  4 |  8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40 |\n" +
-                "  5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 | 50 |\n" +
-                "  6 | 12 | 18 | 24 | 30 | 36 | 42 | 48 | 54 | 60 |\n" +
-                "  7 | 14 | 21 | 28 | 35 | 42 | 49 | 56 | 63 | 70 |\n" +
-                "  8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 72 | 80 |\n" +
-                "  9 | 18 | 27 | 36 | 45 | 54 | 63 | 72 | 81 | 90 |\n" +
-                " 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90 |100 |\n";
- */
-    public static String getMultiplicationTable(int tableSize) {
-
-        StringBuilder str = new StringBuilder();
-        for(int i = 1; i <= tableSize; i++)
-        {
-            for (int j = 1; j <=tableSize; j++) {
-                str.append(String.format("%4d |", i * j));
-            }
-            str.append("\n");
-        }
-        return str.toString();
-
-    }
+    */
 }
